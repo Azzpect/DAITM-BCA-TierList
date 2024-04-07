77ffcca15d1b9781d4710071e5b5e5e38a9214e6
Azzpect <azzpect6571@gmail.com>
2024-04-07 23:52:29 +0530

    fixed mobile compatibility

--- website/public/script.js
+++ website/public/script.js
@@ -5,6 +5,57 @@ document.querySelectorAll(".draggable").forEach(element => {
     element.addEventListener("dragstart", (e) => {
         draggable = e.target;
     });      
+    element.addEventListener("touchstart", e => {
+        draggable = e.target;
+    });
+})
+document.addEventListener("touchmove", e => {
+    e.preventDefault();
+});
+document.addEventListener("touchend", e => {
+    draggable.style.left = e.clientX + 'px';
+    draggable.style.top = e.clientY  + 'px';
+    rows.forEach((row, index) => {
+        let id = row.getAttribute("id")
+        if(e.target.closest(`#${id}`)) {
+            row.appendChild(draggable);
+        }
+    })
+    remaining -= 1;
+    if(remaining == 0) {
+        document.querySelector("#submit").addEventListener("click", (e) => {
+            let name = prompt("Enter your full name:");
+            let data = {Name: name, tier: {
+                S:[],
+                A:[],
+                B:[],
+                C:[],
+                D:[],
+                F:[]
+            }}
+            for(let key in data.tier) {
+                let nominees = document.querySelectorAll(`#${key}-nominee>.draggable`);
+                nominees.forEach((nominee, index) => {
+                    data.tier[key].push(nominee.getAttribute("name"));
+                })
+            }
+            fetch("/send-tier", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({data})
+            }).then(res => {
+                return res.json();
+            }).then(data => {
+                document.querySelector(".message-text").innerHTML = `${data.status.toUpperCase()}! ${data.msg}`;
+                document.querySelector(".message").style.display = "flex";
+            }).catch(err => {
+                console.log(err);
+            })
+            
+        })
+    }
 })
 document.addEventListener("dragover", (e) => {
     e.preventDefault();
